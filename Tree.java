d3dac34054b137669c0a1d77d4cd7808e729edb2
Ebony B <70521133+MiscRep@users.noreply.github.com>
2022-06-30 17:24:41 -0400

    Planting a Tree
    Just a simple program used to introduce me (Rep) to the user and plant a tree for them

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String [] args) {
+     /*This code will introduce me and I'll plant a tree for you*/
+     System.out.println("Hey, I'm Rep and I'm learning to code in Java!");
+     System.out.println("I'm going to plant a tree today!");
+     System.out.println("   *   ");
+     System.out.println("  ***  ");
+     System.out.println("  ***  ");
+     System.out.println("   *   ");
+     System.out.println("   *   ");
+    }
+  }
